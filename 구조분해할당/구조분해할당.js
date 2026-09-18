6b9ef1b77b6b5d8ce51acd0a56416badb025d4f8
최은관 <silvergwan@gmail.com>
2026-09-18 13:09:56 +0900

    feat: 구조분해 할당 실습

---
+++ 구조분해할당/구조분해할당.js
@@ -0,0 +1,10 @@
+// 이름과 성을 요소로 가진 배열
+let arr = ["Eungwan", "Choi"];
+
+// 구조분해 할당을 이용해
+// firstName에는 arr[0]을
+// surname엔 arr[1]을 할당하였습니다.
+let [firstName, surname] = arr;
+
+console.log(firstName);
+console.log(surname);
